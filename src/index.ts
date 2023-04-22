4a7761eeec340def69d686218755fcab31ae68cf
S.Watanabe <ocelot2828@gmail.com>
2023-04-22 23:28:15 +0900

    fix(effect): suppress of flash render

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,51 @@
-import { useState, useEffect, RefObject } from 'react'
 import { useLocation } from 'react-router-dom'
+import { useCallback, useLayoutEffect, useState } from 'react'
+import type { RefObject } from 'react'
 
-export interface FitterOptions {
-  offsetY?: number
+export type Option = {
   offsetX?: number
+  offsetY?: number
+  optional?: RefObject<HTMLElement>
 }
 
-export interface UseFitterResult {
-  isFitted: boolean
-}
-
-const useFitter = (
-  ref: RefObject<HTMLElement>,
-  { offsetY = 0, offsetX = 0 }: Partial<FitterOptions> = {}
-): UseFitterResult => {
-  const [isFitted, setIsFitted] = useState(true)
+const useFitter = ({
+  offsetX = 0,
+  offsetY = 0,
+  optional: ref
+}: Partial<Option> = {}) => {
   const { pathname } = useLocation()
+  const [isFit, setIsFit] = useState(true)
 
-  useEffect(() => {
-    // Create ResizeObserver and watch for changes to the element's dimensions
-    const observer = new ResizeObserver(entries => {
-      entries.forEach(entry => {
-        // Check if element fits within the viewport
-        const windowHeight = window.innerHeight - offsetY
-        const windowWidth = window.innerWidth - offsetX
-        setIsFitted(
-          entry.target.clientHeight < windowHeight &&
-            entry.target.clientWidth < windowWidth
-        )
-      })
-    })
+  // Callback function a this file's core.
+  const updateStatus = useCallback(() => {
+    const winWidth = window.innerWidth - offsetX
+    const winHeight = window.innerHeight - offsetY
+    const height = ref?.current?.clientHeight as number
+    const width = ref?.current?.clientWidth as number
+    setIsFit(height < winHeight && width < winWidth)
+  }, [offsetX, offsetY, ref])
 
-    // Start observing the element's dimensions
-    if (ref.current) observer.observe(ref.current)
+  // Page transition function.
+  useLayoutEffect(() => {
+    return updateStatus()
+  }, [pathname, updateStatus])
 
-    // Clean up the observer when the component unmounts
-    return () => observer.disconnect()
-  }, [offsetX, offsetY, ref, setIsFitted, isFitted, pathname])
-
-  return { isFitted }
+  // Realtime resize event watcher function.
+  useLayoutEffect(() => {
+    // create constructor watch's realtime event.
+    const observer = new ResizeObserver(updateStatus)
+    // start ovserving the element.
+    if (ref?.current) {
+      observer.observe(ref.current)
+    }
+    // clean up the observer when the ref component unmount.
+    return () => {
+      observer.disconnect()
+    }
+  }, [ref, updateStatus])
+  return isFit
 }
 
 export default useFitter
+import { Main } from './container/Main'
+export { Main }
