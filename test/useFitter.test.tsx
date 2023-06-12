5c63484bad63486e93a44af9e79c25705e5ce776
S.Watanabe <ocelot2828@gmail.com>
2023-06-13 08:48:25 +0900

    test: passed class and change a test text

--- test/useFitter.test.tsx
+++ test/useFitter.test.tsx
@@ -3,17 +3,29 @@ import React from 'react'
 import { renderHook, render, screen } from '@testing-library/react'
 
 describe('useFitter', () => {
-  test('if arg is not set a value is undefined', () => {
+  test('if arg is miss matched value return undefined', () => {
     const { result } = renderHook(() => useFitter('test', '/'))
     expect(result.current).toBeUndefined()
   })
 
   const refObject = React.createRef<HTMLDivElement>()
   const TestComponent = () => {
-    return <main>Test Component</main>
+    return <main className="container">Test Component</main>
   }
 
-  test('should return true when the ref object width is Smaller than 1', () => {
+  test('should return true when the element width is Smaller than 1', () => {
+    render(<TestComponent />)
+    const element = screen.getByText('Test Component')
+
+    Object.defineProperty(element, 'clientWidth', { value: 0 })
+    Object.defineProperty(element, 'clientHeight', { value: 0 })
+    const { result } = renderHook(() =>
+      useFitter('.container', '/', { offsetX: -1, offsetY: -1 })
+    )
+    expect(result.current).toBe(true)
+  })
+
+  test('should return true when the element width is Smaller than 1', () => {
     render(<TestComponent />)
     const element = screen.getByText('Test Component')
 
@@ -25,7 +37,7 @@ describe('useFitter', () => {
     expect(result.current).toBe(true)
   })
 
-  test('should return false when the ref object width is greater than 999', () => {
+  test('should return false when the element width is greater than 999', () => {
     render(<TestComponent />)
     const element = screen.getByText('Test Component')
 
