13c350e91cc1d2d60782e4ed6cee2454cca87038
S.Watanabe <ocelot2828@gmail.com>
2023-06-12 22:58:31 +0900

    perf(core): changed usability

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,42 @@
 import { useCallback, useLayoutEffect, useState } from 'react'
-import type { RefObject } from 'react'
 
 type Option = {
   offsetX?: number
   offsetY?: number
 }
 
-type State = boolean | null | undefined
-
 const useFitter = (
-  ref?: RefObject<HTMLElement>,
-  location?: string,
+  tagName: string,
+  pathname: string,
   { offsetX = 0, offsetY = 0 }: Option = {}
 ) => {
-  const [isFit, setIsFit] = useState<State>(undefined)
+  const [isFit, setIsFit] = useState<boolean | undefined>(undefined)
 
-  // Client safe effect.
+  // Client safe.
   const canUseDOM = !!(
     typeof window !== 'undefined' &&
     window.document &&
     window.document.createElement
   )
   const useClientEffect = canUseDOM ? useLayoutEffect : () => {}
+  const elm = canUseDOM && document.getElementsByTagName(tagName as string)[0]
 
   // This the core callback function.
   const updateStatus = useCallback(() => {
-    // skip effect, if ref is undefined
-    if (ref === undefined) return setIsFit(null)
+    // skip effect, if elm and pathname is undefined
+    if (elm === undefined || pathname === undefined) return setIsFit(undefined)
     // window and content size calculation
-    const winWidth = window.innerWidth - offsetX
-    const winHeight = window.innerHeight - offsetY
-    const height = ref?.current?.clientHeight as number
-    const width = ref?.current?.clientWidth as number
-    setIsFit(height <= winHeight && width <= winWidth)
-  }, [offsetX, offsetY, ref])
+    const winWidth = innerWidth - offsetX
+    const winHeight = innerHeight - offsetY
+    const tagHeight = (elm && elm.clientHeight) as number
+    const tagWidth = (elm && elm.clientWidth) as number
+    setIsFit(tagHeight <= winHeight && tagWidth <= winWidth)
+  }, [elm, pathname, offsetX, offsetY])
 
-  // Watches for changes the element size and window size.
-  useClientEffect(() => {
-    // call updateStatus to set initial state
-    updateStatus()
-
-    // entry window resize listener
-    // create constructor and watch's start ovserve
-    window.addEventListener('resize', updateStatus)
-    const observer = new MutationObserver(updateStatus)
-    if (ref?.current)
-      observer.observe(ref.current, {
-        childList: true,
-        subtree: true
-      })
-
-    // Clean up listener and observer on ref component unmount.
-    return () => {
-      window.removeEventListener('resize', updateStatus)
-      observer.disconnect()
-    }
-  }, [updateStatus, location])
+  // Page pathname updated and trigger the Callback and element is re evaluated.
+  useClientEffect(updateStatus, [updateStatus])
 
   return isFit
 }
+
 export default useFitter
-import { Main } from './container/Main'
-export { Main }
